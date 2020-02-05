db1a41236b103b7479301341bcbdf9435a8c702e
Stefan <stefan9283@gmail.com>
2020-02-05 14:34:54 +0200

    Auto-commit at Wed Feb  5 14:34:39 EET 2020

---
+++ 1.windows/windows.c
@@ -0,0 +1,57 @@
+#include </usr/include/GLFW/glfw3.h>
+#include <stdio.h>
+
+/*
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+*/
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	printf("%d\n", key);
+   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main()
+{
+
+
+if (!glfwInit())
+{
+   printf("failed\n");
+    // Initialization failed
+}
+//else printf("successful\n");
+
+//double time = glfwGetTime();
+GLFWwindow* window = glfwCreateWindow(1920, 1080, "My Title", NULL, NULL);
+glfwMakeContextCurrent(window);
+glfwSetKeyCallback(window, key_callback);
+    while (!glfwWindowShouldClose(window))
+	{
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+		glfwSwapBuffers(window);
+		glfwSwapInterval(100);
+
+		glfwPollEvents();
+
+		//scanf("%d",&c);
+		if (!window)
+		{
+			   printf("failed\n");
+
+		    // Window or OpenGL context creation failed
+		}
+		//else printf("successful\n");
+	}
+glfwDestroyWindow(window);
+
+glfwTerminate();
+
+return 0;
+}
--- Makefile
+++
@@ -1,7 +0,0 @@
-
-NAME=windows
-
-build:
-	gcc $(NAME).c -o $(NAME)  -lglfw   -lglut -lGL -lGLEW -lGLU -Wall # -lGL -lglfw3 -lXrandr -lglfw -lXi -lX11 -lXxf86vm -lpthread 
-	./$(NAME)
-	
