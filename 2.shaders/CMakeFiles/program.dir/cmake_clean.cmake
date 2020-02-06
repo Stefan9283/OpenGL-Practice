e87855f63b774ff27bb8e32918d4f4b9fee245bb
Stefan <stefan9283@gmail.com>
2020-02-06 02:49:29 +0200

    Auto-commit at Thu Feb  6 02:49:16 EET 2020

---
+++ 2.shaders/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/src/glad.c.o"
+  "CMakeFiles/program.dir/main.c.o"
+  "program.pdb"
+  "program"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
