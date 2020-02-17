dc41c8798ce9964de753b641e1da657d616edd02
Stefan <stefan9283@gmail.com>
2020-02-17 22:56:45 +0200

    Auto-commit at Mon Feb 17 22:56:34 EET 2020

---
+++ 3.rectangle_indexed_vertices/C++/CMakeFiles/testnr1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testnr1.dir/src/glad.c.o"
+  "CMakeFiles/testnr1.dir/main.cpp.o"
+  "testnr1.pdb"
+  "testnr1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/testnr1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
