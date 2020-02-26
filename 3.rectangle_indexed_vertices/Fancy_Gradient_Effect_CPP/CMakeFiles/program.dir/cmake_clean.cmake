13ef52c58854f0f8cf88d94f125a2ab8fd5af3bc
Stefan <stefan9283@gmail.com>
2020-02-27 01:39:22 +0200

    Auto-commit at Thu Feb 27 03:39:07 EET 2020

---
+++ 3.rectangle_indexed_vertices/Fancy_Gradient_Effect_CPP/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/src/glad.c.o"
+  "CMakeFiles/program.dir/main.cpp.o"
+  "program.pdb"
+  "program"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
